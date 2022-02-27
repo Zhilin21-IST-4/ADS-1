337133c154ede27a202f189fb0ffbb968fdec31d
Zhilin21-IST-4 <94217280+Zhilin21-IST-4@users.noreply.github.com>
2022-02-27 16:12:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -40,8 +40,8 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
   // вставьте код функции
-    int sum = 0;
-  for (int i = 0; i < hbound; i++) {
+  uint64_t sum = 0;
+  for (int i = 2; i < hbound; i++) {
     if (checkPrime(i)) {
       sum += i;
     }
